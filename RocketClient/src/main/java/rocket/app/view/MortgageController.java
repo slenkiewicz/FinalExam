94e6883793ab212e4d3c866063ec6c3c984d4b0f
Steven <Steven@Steven-HP>
2016-05-23 21:56:17 -0400

    RocketClient

---
+++ RocketClient/src/main/java/rocket/app/view/MortgageController.java
@@ -0,0 +1,71 @@
+package rocket.app.view;
+
+import eNums.eAction;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.shape.Rectangle;
+import rocket.app.MainApp;
+import rocketCode.Action;
+import rocketData.LoanRequest;
+
+public class MortgageController {
+	@FXML
+	private MainApp mainApp;
+	private TextField txtIncome;
+	private TextField txtExpenses;
+	private TextField txtCreditScore;
+	private TextField txtHouseCost;
+	private ComboBox loanterm;
+	private Label Income;
+	private Label Expenses;
+	private Label CreditScore;
+	private Label HouseCost;
+	private Button Calculate;
+	private Label MortgagePayment;
+	
+	ObservableList<String> options = 
+		    FXCollections.observableArrayList(
+		        "30 years",
+		        "15 years");
+		final ComboBox box = new ComboBox(options);
+		
+	public void setMainApp(MainApp mainApp) {
+		this.mainApp = mainApp;
+	}
+	
+	//	TODO - RocketClient.RocketMainController
+	//			Call this when btnPayment is pressed, calculate the payment
+	@FXML
+	public void btnCalculatePayment(ActionEvent event)
+	{
+		Object message = null;
+		//	TODO - RocketClient.RocketMainController
+		
+		Action a = new Action(eAction.CalculatePayment);
+		LoanRequest lq = new LoanRequest();
+		//	TODO - RocketClient.RocketMainController
+		//			set the loan request details...  rate, term, amount, credit score, downpayment
+		//			I've created you an instance of lq...  execute the setters in lq
+
+		a.setLoanRequest(lq);
+		
+		//	send lq as a message to RocketHub		
+		mainApp.messageSend(lq);
+	}
+	
+	public void HandleLoanRequestDetails(LoanRequest lRequest)
+	{
+		//	TODO - RocketClient.HandleLoanRequestDetails
+		//			lRequest is an instance of LoanRequest.
+		//			after it's returned back from the server, the payment (dPayment)
+		//			should be calculated.
+		//			Display dPayment on the form, rounded to two decimal places
+		
+	}
+}
